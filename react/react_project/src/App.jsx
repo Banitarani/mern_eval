f7ac3cb746fd34aeb98769b87ff0d34dafb4674b
Banita Das <banitaranidas8@gmail.com>
2024-12-23 14:05:55 -0500

    this is hello project

---
+++ react/react_project/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+import Hello from './componenets/Hello'
+
+function App() {
+  
+<Hello>
+
+</Hello>
+ 
+}
+
+export default App
---
+++ react/react_project/src/componenets/Hello.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const Hello = () => {
+  return (
+    <div>
+      <p>hello kids</p>
+    </div>
+  );
+};
+
+export default Hello;
